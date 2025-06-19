538659c1789632c6f87bd145118a56e647df95b1
Samuel David Roncal Vidal <davidroncal@MacBook-Pro-de-Samuel-2.local>
2025-06-19 17:26:39 -0500

    agregar index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, '', redirect);
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
